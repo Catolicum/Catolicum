8b13c3e0d6c1af7d100323a7531a3ec75b52a23d
Catolicum <catolicum@outlook.com>
2026-03-30 22:31:50 +0200

    fix: next-pwa config para Next.js 14

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
+/** @type {import('next').NextConfig} */
+
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
+  buildExcludes: [/middleware-manifest\.json$/],
 });
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
